10fb7672993c95e8846fe5cd57bbed2dcdf065ce
Abdullah Zahid <abdullahzahid1796@gmail.com>
2025-04-15 01:47:01 +0500

    Game Mechanics of Level 01 implemented
    Player's movement
    Green and Red Light timers
    Elimination detection
    Endpoint detection

---
+++ Assets/3. Scripts/LightManager.cs
@@ -0,0 +1,56 @@
+using TMPro;
+using UnityEngine;
+
+public class LightManager : MonoBehaviour
+{
+    //values
+    [SerializeField] private GameObject player;
+    [SerializeField] private float greenLightTime = 5.0f;
+    [SerializeField] private float redLightTime = 2.0f;
+
+    //canvas references
+    [SerializeField] private TMP_Text alert;
+
+    //script variables
+    private bool greenLight = true;
+    private float time = 0;
+    private Vector3 oldPos;
+
+    
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if(time >= greenLightTime - 3)
+        {
+            alert.text = ((int)(greenLightTime - time)).ToString();
+        }
+
+        if(time >= greenLightTime && greenLight)
+        {
+            time = 0;
+            greenLight = false;
+            oldPos = player.transform.position;
+            alert.text = "Stop";
+        }
+
+        if(time >= redLightTime && !greenLight)
+        {
+            time = 0;
+            greenLight = true;
+            alert.text = string.Empty;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(!greenLight)
+        {
+            if(oldPos != player.transform.position)
+            {
+                alert.text = "Game Over";
+                Time.timeScale = 0;
+            }
+        }
+    }
+}
---
+++ Assets/3. Scripts/PlayerMovement.cs
@@ -0,0 +1,83 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //input references
+    [SerializeField] private InputActionReference movement;
+    [SerializeField] private InputActionReference jump;
+
+
+    //values
+    [SerializeField] private float maxSpeed = 3.0f;
+    [SerializeField] private float moveSpeed = 2.0f;
+    [SerializeField] private float acceleration = 10.0f;
+    [SerializeField] private float jumpForce = 10.0f;
+
+    //script variables
+    private Rigidbody rb;
+    private bool onGround = true;
+
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate()
+    {
+        //reading value from input system
+        Vector2 move = movement.action.ReadValue<Vector2>();
+
+        //generating velocity
+        Vector3 velocity = new Vector3(move.x, 0, move.y) * moveSpeed * Time.fixedDeltaTime * acceleration;
+
+        //adding force
+        rb.AddForce(velocity, ForceMode.Impulse);
+
+        //limiting force to maxSpeed from increasing with time
+        Vector3 newVelocity = rb.linearVelocity;
+        Vector3 limitVelocity = Vector3.ClampMagnitude(new Vector3(newVelocity.x, 0f, newVelocity.z), maxSpeed);
+        rb.linearVelocity = new Vector3 (limitVelocity.x, newVelocity.y, limitVelocity.z);
+        //Debug.Log(rb.linearVelocity.x + " , " + rb.linearVelocity.z);
+    }
+
+    void Jump(InputAction.CallbackContext context)
+    {
+        if(onGround)
+        {
+            onGround = false;
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnEnable()
+    {
+        jump.action.started += Jump;
+    }
+
+    private void OnDisable()
+    {
+        jump.action.started -= Jump;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            onGround = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Endpoint"))
+        {
+            Time.timeScale = 0;
+            Debug.Log("Level Accomplished!");
+            //move to next level
+        }
+    }
+}
